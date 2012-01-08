5c32a1edf2ad2e6b2cb8fa848d94f062bb661d8b
Rajaram Mallya and Gavri Fernandez <rax_network_service+rmallya+gavri@thoughtworks.com>
2012-01-08 10:13:28 +0530

    First

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,7 @@
+class ChatsController < ApplicationController
+
+  def main
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chat::Application.config.secret_token = '792597fa00c84c5e5936d65f1aece0c45ca8e75758787c7594df3bc61932a54c3cc65e3e9b299d4e8556f7c1d0a12dc24c8403bcff0b23fab732b903d05b9b8d'
